de4d17e665abc305afecced2750cd813f8b6b524
luis <gutierrez.luis@icloud.com>
2025-04-13 19:17:54 -0700

    Add files via upload

--- class_method_parameter.py
+++ class_method_parameter.py
@@ -8,13 +8,15 @@ class Drone:
 a = Drone("Vancouver")
 print(a.return_weapon_and_target("Missile"))
 
+target = input("Enter a target:")
+
+
 class Drone:
     def __init__(self, weapon):
         self.weapon = weapon
     def destroy(self, target):
-        target = input("Enter a target: ")
         return f"The drone will destroy {target} with a {self.weapon}"
 
 
 a = Drone("Machine Gun")
-print(a.destroy("target"))
+print(a.destroy(target))
