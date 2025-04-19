d08c24a45d22ebbcacf413d30f71d9ccefab3673
luis <gutierrez.luis@icloud.com>
2025-04-18 17:24:30 -0700

    Add files via upload

--- wishlist.py
+++ wishlist.py
@@ -16,13 +16,13 @@ random.shuffle(books)
 
 def x(books):
     r = random.choice(books)
-    print(f"\n{r}")
+    print(f"\nOnly one from the list: {r}")
+
 
 x(books)
-
-
 def c(books):
     n = random.sample(books, 2)# get 2 random items from the list
-    return f"{n}"
+    return f"\n{len(n)} random items from the list: {n}"
 
 
+print(c(books))
