a5cb770c5be7a8451ca30c71c8c7eae65beee68d
luis <gutierrez.luis@icloud.com>
2025-04-18 17:15:06 -0700

    Add files via upload

---
+++ wishlist.py
@@ -0,0 +1,28 @@
+import random
+
+
+books = [
+        "Automate the Boring Stuff with Python",
+        "Python for Data Analysis",
+        "Fluent Python",
+        "Python for kids",
+        "Hello Web App",
+]
+
+print("Suggested gift: {}".format(books[0]))
+
+random.shuffle(books)
+#print(f"\n{books}")
+
+def x(books):
+    r = random.choice(books)
+    print(f"\n{r}")
+
+x(books)
+
+
+def c(books):
+    n = random.sample(books, 2)# get 2 random items from the list
+    return f"{n}"
+
+
