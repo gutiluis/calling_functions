f0b334fe44f4b69f4c822ea174ad4312778d6ba9
luis <gutierrez.luis@icloud.com>
2025-04-21 17:46:43 -0700

    Add files via upload

--- deletion.py
+++ deletion.py
@@ -1,22 +1,42 @@
-def x():
-    appender = []
-    while True:
-        wea = input("Enter weapons: ")
-        if len(wea) >= 1:
-            print("Using: {}".format(wea))
-        elif len(wea) < 1:
-            break
-        appender.append(wea)    
+def write():
+    with open("test.txt", "w") as f:
+        def x():
+            appender = []
+            while True:
+                wea = input("Enter weapons: ")
+                if wea == "exit":
+                    break
+                elif len(wea) >= 1:
+                    print("Using: {}".format(wea))
+                    appender.append(wea)
+                elif not wea:
+                    break       
+            print("Using {}".format(appender))
+            f.write("\n".join(appender)) # save to file
+        x()
+    f.close()
+
 
 def k():
     tar = [
         "vancouver",
         "richmond", #always use comma at the last element in a list value last index
     ]
+    print(f"These are the selected targets: {tar}")
     if tar[0]:
         print("Attack {}.".format(tar[0]))
         if tar[1]:
             print("Try {} after.".format(tar[1]))
+    def forg():
+        prompt = "Want to extend the list? "
+        extend_targets = input(prompt)
+        if extend_targets == "no":
+            pass
+        else:
+            tar.extend("Calgary")
+            print(tar)
+        return
+    forg()
 
 def emoji():
     lunch = "\N{TACO}"
@@ -32,7 +52,7 @@ def time_of_attack():
 
 
 if __name__=="__main__":
-    x()
+    write()
     k()
     emoji()
     time_of_attack()
