126ddd96cbddf74669cfeb9d5109463fc8faee5f
luis <gutierrez.luis@icloud.com>
2025-05-03 20:17:04 -0700

    Add files via upload

--- make_a_binary.py
+++ make_a_binary.py
@@ -1,7 +1,16 @@
 def decimal_then_binary():
-	x = ord(input("Enter a single letter:"))
-	c = bin(x)
-	return c
+	# this makes the input a string either way
+	x = input("Enter a single letter: ")
+	if x.isdigit():
+		raise Exception("Do not enter an integer")
+	elif not x:
+		raise Exception("Enter something")
+	elif len(x) > 1:
+		raise Exception("Enter only 1 character")
+	else:
+		make_decimal = ord(x)
+		make_binary = bin(make_decimal)
+		print(make_binary)
 
 
 decimal_then_binary()
