2c6844694d2444f73d1d019109bcf6415e725c7c
luis <gutierrez.luis@icloud.com>
2025-05-04 20:00:34 -0700

    Add files via upload

--- generator.py
+++ generator.py
@@ -1,6 +1,8 @@
-x = input("Enter something to reverse: ")
+# self.index
+# self.data
 
-
+y = input("Enter a path: ")
+x = input("Enter something to reverse with a generator iterator: ")
 
 def reverse(data):
     for index in range(len(data)-1, -1, -1):
@@ -8,7 +10,7 @@ def reverse(data):
 
 # use a string
 for char in reverse(x):
-	print(char)
-
+    with open(y, "a") as file:
+        file.write(char + "\n")
 
 
